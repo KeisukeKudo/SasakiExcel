047ff2898bf35215c6d60aa999fb87a31367bfc9
KeisukeKudo <melchior.balthasar.casper326@gmail.com>
2016-06-18 01:39:56 +0900

    sheetのusingし忘れ､実行時間表示

--- SasakiExcel/Program.cs
+++ SasakiExcel/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using System.Diagnostics;
 
 namespace SasakiExcel {
     class Program {
@@ -30,11 +31,14 @@ namespace SasakiExcel {
                 Console.ReadLine();
                 return;
             }
-            
+
             try {
+                var s = new Stopwatch();
+                s.Start();
                 Console.WriteLine(" 処理中です.........\r\n");
                 ImageExcelToCopy();
-                Console.WriteLine(" 正常終了しました");
+                s.Stop();
+                Console.WriteLine(" 正常終了しました({0:N0}ms)", s.ElapsedMilliseconds);
 
             } catch (Exception e) {
                 Console.WriteLine(" 異常終了しました");
@@ -51,29 +55,27 @@ namespace SasakiExcel {
         /// 対象画像のピクセルから色を取得し､Excelシートの各セルに設定､保存
         /// </summary>
         static void ImageExcelToCopy() {
-            DeleteFile();
+            if (File.Exists(ExcelFileName)) {
+                File.Delete(ExcelFileName);
+            }
 
             //Excelファイル作成
             var outputFile = new FileInfo(ExcelFileName);
-
-            //画像をBitmapで取得
-            //Excelファイルを開く
+            
             using (var bitmap = new Bitmap(ImageFileName))
-            using (var book = new ExcelPackage(outputFile)) {
+            using (var book = new ExcelPackage(outputFile))
+            using (var sheet = book.Workbook.Worksheets.Add(SheetName)) { 
                 //回転情報が無いのに回転してしまうので正しい位置に調整
                 //時計回りに90度回転し､水平方向に反転
                 bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
-
-                //シートを作成
-                var sheet = book.Workbook.Worksheets.Add(SheetName);
                 for (var y = 1; y <= bitmap.Height; y++) {
                     //高さ指定
                     sheet.Row(y).Height = 3.8;
                     for (var x = 1; x <= bitmap.Width; x++) {
-                        //ピクセルの色を取得
-                        var color = bitmap.GetPixel(x - 1, y - 1);
                         //セルの背景色に設定
                         var cell = sheet.Cells[x, y];
+                        //ピクセルの色を取得
+                        var color = bitmap.GetPixel(x - 1, y - 1);
                         SetBackgroundColor(cell, color);
 
                         //横幅指定
@@ -95,15 +97,5 @@ namespace SasakiExcel {
             cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
             cell.Style.Fill.BackgroundColor.SetColor(color);
         }
-
-        /// <summary>
-        /// 既にファイルが存在している場合は削除
-        /// </summary>
-        static void DeleteFile() {
-            if (File.Exists(ExcelFileName)) {
-                File.Delete(ExcelFileName);
-            }
-        }
-
     }
 }
